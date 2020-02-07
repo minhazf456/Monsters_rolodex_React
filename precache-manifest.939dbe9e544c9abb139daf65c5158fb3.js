585d356cbe867f85296a6303c7bd9d4bf0977010
minhazf456 <minhazf@gmail.com>
2020-02-07 03:51:52 -0800

    Updates

---
+++ precache-manifest.939dbe9e544c9abb139daf65c5158fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ab3e02de24c692a82004d2a4251569",
+    "url": "/Monsters_rolodex_React/index.html"
+  },
+  {
+    "revision": "1b96b4794aea7140223f",
+    "url": "/Monsters_rolodex_React/static/css/main.d2d61b28.chunk.css"
+  },
+  {
+    "revision": "a9efd0cc9c86482af97b",
+    "url": "/Monsters_rolodex_React/static/js/2.b4f66f1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters_rolodex_React/static/js/2.b4f66f1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b96b4794aea7140223f",
+    "url": "/Monsters_rolodex_React/static/js/main.18d5db4b.chunk.js"
+  },
+  {
+    "revision": "f388611468de6fe9f868",
+    "url": "/Monsters_rolodex_React/static/js/runtime-main.784283dd.js"
+  }
+]);
