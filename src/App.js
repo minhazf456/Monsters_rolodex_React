ffa7a7d318c41e21038a7da3865d7f5bebbfc8a3
minhazf456 <minhazf@gmail.com>
2020-02-06 23:05:55 -0800

    working with DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component { // component is a property on react
+  constructor() {
+    super();
+    this.state = {
+      string : "hellow Minhaz"
+    };
+  }
+
+
+render() {  // this builtin render method directly comes from the Reacts component library
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> {this.state.string} </p>
+        <button onClick={()=> this.setState ({ string:" Minhaz Haque" })} >
+        change text </button>
       </header>
     </div>
   );
+}
 }
 
 export default App;
