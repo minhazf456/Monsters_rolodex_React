0e50de39668de800fe408a3b14bf052087410019
minhazf456 <minhazf@gmail.com>
2020-02-07 01:56:27 -0800

    added component list

--- src/App.js
+++ src/App.js
@@ -14,31 +14,29 @@ class App extends Component { // component is a property on react
   //   // we're using this componentdidmount lifecycle method that we have access to because of our class component and we are 
   // fetching from this URL we are taking a response and converting it into the Jason format that are JavaScript can understand and use and 
   // then we're going to take the users that we got back from it and set our monsters to that array of user and that's how
-  // we end up with this list of names from the API
-
+  // we end up with this list of in the next section we're going to now convert this to look a little bit closer to this.
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({
-        monsters: users
-      }));
-
+      .then(users => this.setState({ monsters: users }));
   }
-  render() { // this built in render method directly comes from the Reacts component library
-    return ( <
-      div className = "App" > {
-        this.state.monsters.map(monsters => ( <
-          h1 key = {
-            monsters.id
-          } > {
-            monsters.name
-          } < /h1>
-        ))
-      }
 
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
 
-      <
-      /div>
+  render() {  // this is the built in method directly comes from the react component library
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
     );
   }
 }
